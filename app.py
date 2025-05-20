64baa13d422961c8fd826bb92e0ce7b9f21f590f
Md Atif <141242287+Atif2292@users.noreply.github.com>
2025-05-21 02:12:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -167,6 +167,8 @@ def get_recommendations():
         print("🔥 Error in recommendations:", str(e))
         return jsonify({"error": str(e)}), 500
 if __name__ == '__main__':
+    import os
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
